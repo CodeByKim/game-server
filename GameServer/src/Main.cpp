88f2445b1f6ec6294c9a3aa80a4b6ce8cfb3429a
Garam Kim <codebykim@naver.com>
2021-07-29 12:14:02 +0900

    Logger 연동 성공

--- GameServer/src/Main.cpp
+++ GameServer/src/Main.cpp
@@ -6,8 +6,30 @@
 #define SERVER_PORT 6000
 #define MAX_CCU 3000
 
+void init_logger()
+{
+	Log::Configure networkLogConfig;
+	networkLogConfig.SetLoggerName(L"Network");
+	networkLogConfig.SetWriter(Log::eLogWriter::Console);	
+	networkLogConfig.SetWriter(Log::eLogWriter::File);
+
+	Log::Configure gameLogConfig;
+	gameLogConfig.SetLoggerName(L"Game");
+	gameLogConfig.SetWriter(Log::eLogWriter::Console);
+	gameLogConfig.SetWriter(Log::eLogWriter::File);
+
+	Log::Manager::Create(&networkLogConfig);
+	Log::Manager::Create(&gameLogConfig);
+}
+
 int main()
 {			
+	init_logger();
+
+	LOG_INFO(L"Network") << 1;
+	LOG_ERROR(L"Game") << 2;
+	LOG_INFO(L"Network") << 3;
+
 	//garam::net::NetServer server(SERVER_PORT, MAX_CCU);
 	garam::net::GameServer server(SERVER_PORT, MAX_CCU);	
 	server.RegisterMessageHandler(new EchoMessageHandler());
