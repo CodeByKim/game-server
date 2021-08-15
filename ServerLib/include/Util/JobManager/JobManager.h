a75ecaafa0a325124bfe9a1f43e91139eeaf2978
Garam Kim <codebykim@naver.com>
2021-08-15 21:36:51 +0900

    네임스페이스 변경

--- ServerLib/include/Util/JobManager/JobManager.h
+++ ServerLib/include/Util/JobManager/JobManager.h
@@ -3,80 +3,83 @@
 
 #define JOB_INFINITY -1
 
-namespace jobsystem
+namespace garam
 {
-	/*
-	 * IJob을 상속받고 가상함수를 구현
-	 * 스케줄러에 의해 실행 됨
-	 */
-	class IJob
+	namespace jobsystem
 	{
-	public:
-		virtual void Execute() = 0;
-	};
+		/*
+		 * IJob을 상속받고 가상함수를 구현
+		 * 스케줄러에 의해 실행 됨
+		 */
+		class IJob
+		{
+		public:
+			virtual void Execute() = 0;
+		};
 
-	/*
-	 * 등록된 IJob은 Node에 탑재되어 실행 됨
-	 * IJob을 그대로 가지고 핸들링 하면 안됨
-	 * 등록된 Job은 맘대로 삭제할 수 없음
-	 */
-	class JobNode
-	{
-	public:
-		JobNode(IJob* job, int freq, int count);
-		~JobNode();
+		/*
+		 * 등록된 IJob은 Node에 탑재되어 실행 됨
+		 * IJob을 그대로 가지고 핸들링 하면 안됨
+		 * 등록된 Job은 맘대로 삭제할 수 없음
+		 */
+		class JobNode
+		{
+		public:
+			JobNode(IJob* job, int freq, int count);
+			~JobNode();
 
-		void Execute();
-		bool IsExfired();
+			void Execute();
+			bool IsExfired();
 
-	private:
-		IJob* mJob;
-		int mFreq;
-		int mCount;
+		private:
+			IJob* mJob;
+			int mFreq;
+			int mCount;
 
-		DWORD mNextTick;		
+			DWORD mNextTick;
 
-	};
+		};
 
-	/*
-	 * Job들을 실행하는 워커
-	 * 다중 스레드
-	 */
-	class Worker
-	{
-	public:
-		Worker();
-		~Worker();
+		/*
+		 * Job들을 실행하는 워커
+		 * 다중 스레드
+		 */
+		class Worker
+		{
+		public:
+			Worker();
+			~Worker();
 
-		void AppendJob(IJob* job, int freq, int count);
-		void Run();
-		void Release();
+			void AppendJob(IJob* job, int freq, int count);
+			void Run();
+			void Release();
 
-	private:		
-		std::mutex mLock;
-		std::list<JobNode*> mJobNodes;
-		std::thread mThread;
-		bool mIsExit;
-	};
+		private:
+			std::mutex mLock;
+			std::list<JobNode*> mJobNodes;
+			std::thread mThread;
+			bool mIsExit;
+		};
 
-	/*
-	 * Job을 등록하고 사용하기위한 매니저
-	 * 사용자 인터페이스
-	 */
-	class JobManager
-	{
-	public:
-		JobManager();
-		~JobManager();
+		/*
+		 * Job을 등록하고 사용하기위한 매니저
+		 * 사용자 인터페이스
+		 */
+		class JobManager
+		{
+		public:
+			JobManager();
+			~JobManager();
 
-		void Register(IJob* job, int freq, int count);
-		void Release();
+			void Register(IJob* job, int freq, int count);
+			void Release();
 
-		static JobManager& GetInstance();
+			static JobManager& GetInstance();
 
-	private:
-		std::mutex mLock;
-		int mWorkerCount;
-		std::vector<Worker*> mWorkers;
-	};
+		private:
+			std::mutex mLock;
+			int mWorkerCount;
+			std::vector<Worker*> mWorkers;
+		};
+	}
 }
--- ServerLib/src/Util/JobManager/JobManager.cpp
+++ ServerLib/src/Util/JobManager/JobManager.cpp
@@ -1,152 +1,155 @@
 #include "./Util/JobManager/JobManager.h"
 
-namespace jobsystem
+namespace garam
 {
-	JobNode::JobNode(IJob* job, int freq, int count)
-		: mJob(job)
-		, mFreq(freq)
-		, mCount(count)
-		, mNextTick(timeGetTime() + mFreq)		
-	{		
-	}
-
-	JobNode::~JobNode()
+	namespace jobsystem
 	{
-	}
-	
-	void JobNode::Execute()
-	{						
-		DWORD current = timeGetTime();
-		
-		if (current >= mNextTick)
-		{			
-			mNextTick = timeGetTime() + mFreq;
-			mJob->Execute();
-			mCount -= 1;
-		}
-	}
-
-	bool JobNode::IsExfired()
-	{
-		if (mCount == JOB_INFINITY)
+		JobNode::JobNode(IJob* job, int freq, int count)
+			: mJob(job)
+			, mFreq(freq)
+			, mCount(count)
+			, mNextTick(timeGetTime() + mFreq)
 		{
+		}
+
+		JobNode::~JobNode()
+		{
+		}
+
+		void JobNode::Execute()
+		{
+			DWORD current = timeGetTime();
+
+			if (current >= mNextTick)
+			{
+				mNextTick = timeGetTime() + mFreq;
+				mJob->Execute();
+				mCount -= 1;
+			}
+		}
+
+		bool JobNode::IsExfired()
+		{
+			if (mCount == JOB_INFINITY)
+			{
+				return false;
+			}
+
+			if (mCount == 0)
+			{
+				return true;
+			}
+
 			return false;
 		}
 
-		if (mCount == 0)
+		Worker::Worker()
 		{
-			return true;
+			mIsExit = false;
+
+			mThread = std::thread(&Worker::Run, this);
 		}
 
-		return false;
-	}
-
-	Worker::Worker()
-	{
-		mIsExit = false;
-
-		mThread = std::thread(&Worker::Run, this);
-	}
-
-	Worker::~Worker()
-	{
-		for (JobNode* node : mJobNodes)
-		{			
-			delete node;
-		}
-
-		mJobNodes.clear();
-	}
-
-	void Worker::AppendJob(IJob* job, int freq, int count)
-	{
-		JobNode* node = new JobNode(job, freq, count);		
-
-		mLock.lock();
-		mJobNodes.push_back(node);
-		mLock.unlock();
-	}
-
-	void Worker::Run()
-	{		
-		while (!mIsExit)
-		{			
-			std::vector<JobNode*> deleteJobs;
-
-			mLock.lock();
+		Worker::~Worker()
+		{
 			for (JobNode* node : mJobNodes)
 			{
-				if (node->IsExfired())
-				{
-					deleteJobs.push_back(node);
-					continue;
-				}
-
-				node->Execute();
-			}
-			mLock.unlock();
-
-			for (JobNode* node : deleteJobs)
-			{
-				mJobNodes.remove(node);
 				delete node;
 			}
 
-			Sleep(0);
-		}
-	}
-
-	void Worker::Release()
-	{
-		mIsExit = true;
-		mThread.join();
-	}
-
-	JobManager::JobManager()
-	{
-		mWorkerCount = 4;
-
-		for (int i = 0; i < mWorkerCount; i++)
-		{
-			Worker* worker = new Worker();
-			mWorkers.push_back(worker);
-		}		
-	}
-
-	JobManager::~JobManager()
-	{
-		for (int i = 0; i < mWorkerCount; i++)
-		{
-			delete mWorkers[i];
+			mJobNodes.clear();
 		}
 
-		mWorkers.clear();
-	}
-
-	void JobManager::Register(IJob* job, int freq, int count)
-	{
-		static int nodeCount = 0;
-		int index = nodeCount % mWorkerCount;
-
-		mLock.lock();
-		Worker* worker = mWorkers[index];
-		mLock.unlock();
-
-		worker->AppendJob(job, freq, count);
-		nodeCount += 1;
-	}
-
-	void JobManager::Release()
-	{
-		for (int i = 0; i < mWorkerCount; i++)
+		void Worker::AppendJob(IJob* job, int freq, int count)
 		{
-			mWorkers[i]->Release();
-		}
-	}
+			JobNode* node = new JobNode(job, freq, count);
 
-	JobManager& JobManager::GetInstance()
-	{
-		static JobManager instance;
-		return instance;
+			mLock.lock();
+			mJobNodes.push_back(node);
+			mLock.unlock();
+		}
+
+		void Worker::Run()
+		{
+			while (!mIsExit)
+			{
+				std::vector<JobNode*> deleteJobs;
+
+				mLock.lock();
+				for (JobNode* node : mJobNodes)
+				{
+					if (node->IsExfired())
+					{
+						deleteJobs.push_back(node);
+						continue;
+					}
+
+					node->Execute();
+				}
+				mLock.unlock();
+
+				for (JobNode* node : deleteJobs)
+				{
+					mJobNodes.remove(node);
+					delete node;
+				}
+
+				Sleep(0);
+			}
+		}
+
+		void Worker::Release()
+		{
+			mIsExit = true;
+			mThread.join();
+		}
+
+		JobManager::JobManager()
+		{
+			mWorkerCount = 4;
+
+			for (int i = 0; i < mWorkerCount; i++)
+			{
+				Worker* worker = new Worker();
+				mWorkers.push_back(worker);
+			}
+		}
+
+		JobManager::~JobManager()
+		{
+			for (int i = 0; i < mWorkerCount; i++)
+			{
+				delete mWorkers[i];
+			}
+
+			mWorkers.clear();
+		}
+
+		void JobManager::Register(IJob* job, int freq, int count)
+		{
+			static int nodeCount = 0;
+			int index = nodeCount % mWorkerCount;
+
+			mLock.lock();
+			Worker* worker = mWorkers[index];
+			mLock.unlock();
+
+			worker->AppendJob(job, freq, count);
+			nodeCount += 1;
+		}
+
+		void JobManager::Release()
+		{
+			for (int i = 0; i < mWorkerCount; i++)
+			{
+				mWorkers[i]->Release();
+			}
+		}
+
+		JobManager& JobManager::GetInstance()
+		{
+			static JobManager instance;
+			return instance;
+		}
 	}
 }
