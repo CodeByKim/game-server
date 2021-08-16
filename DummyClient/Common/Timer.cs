3a892f4d8d655f55b1397ed995d47f317bc2df8e
Garam Kim <codebykim@naver.com>
2021-08-16 15:25:38 +0900

    더미 클라이언트 타이머 timeGetTime()으로 변경

--- DummyClient/Common/Timer.cs
+++ DummyClient/Common/Timer.cs
@@ -3,29 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 class Timer
 {
-    private int mCurrentTick;
-    private int mPrevTick;
+    [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+    public static extern uint timeBeginPeriod(uint uMilliseconds);
 
-    private int mDeltaTime;
+    [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+    public static extern uint timeEndPeriod(uint uMilliseconds);
+
+    [DllImport("winmm.dll", EntryPoint = "timeGetTime")]
+    public static extern uint timeGetTime();
+
+    private uint mCurrentTick;
+    private uint mPrevTick;
+
+    private uint mDeltaTime;
 
     public Timer()
     {
-        mCurrentTick = Environment.TickCount;
-        mPrevTick = Environment.TickCount;
+        timeBeginPeriod(1);
+        
+        mCurrentTick = timeGetTime();
+        mPrevTick = timeGetTime();
+
+    }
+
+    ~Timer()
+    {
+        timeEndPeriod(1);
     }
 
     public void Update()
-    {
-        mCurrentTick = Environment.TickCount;
+    {        
+        mCurrentTick = timeGetTime();
         mDeltaTime = mPrevTick - mCurrentTick;
         mPrevTick = mCurrentTick;
     }
 
     public int GetDeltaTime()
     {
-        return mDeltaTime * -1;
+        return (int)(mDeltaTime * -1);
     }
 }
