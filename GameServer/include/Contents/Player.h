cb5e8d853fe5eb938ad3ef62ea38fe44ee37b95a
Garam Kim <codebykim@naver.com>
2021-08-21 19:46:54 +0900

    주석 정리

--- GameServer/include/Contents/Player.h
+++ GameServer/include/Contents/Player.h
@@ -2,8 +2,8 @@
 #include <NetworkLib.h>
 
 #include "./Common/GameCommon.h"
-//#include "./Contents/Entity.h"
 #include "./Contents/BasePlayer.h"
+
 class Sector;
 class RPGGameLogic;
 
@@ -16,18 +16,10 @@ public:
 	void Initialize(garam::net::ClientInfo* client, Position2D pos, RPGGameLogic* gameLogic);	
 	void MoveStart(BYTE dir, float x, float y);
 	void MoveEnd(BYTE dir, float x, float y);
-	void Teleport(BYTE dir, float x, float y);	
-	//garam::net::ClientInfo* GetClientInfo();		
-	//void OnSectorChanged(std::vector<Sector*>& leave, std::vector<Sector*>& enter) override;
-	//bool IsMove();
+	void Teleport(BYTE dir, float x, float y);		
 
 	void OnUpdate(float deltaTime) override;
 	void OnHit(int damage) override;
-private:
-	//void ProcessLeaveSector(std::vector<Sector*>& leaveSectors);
-	//void ProcessNewEnterSector(std::vector<Sector*>& enterSectors);
-
-	RPGGameLogic* mGameLogic;
-	//garam::net::ClientInfo* mClient;
-	//bool mIsMoving;	
+private:	
+	RPGGameLogic* mGameLogic;	
 };
