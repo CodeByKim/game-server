32e5b6a576a9d9ba3e670da28c653f15e796544b
Garam Kim <codebykim@naver.com>
2021-08-21 18:20:20 +0900

    baseplayer 수정 중

--- GameServer/include/Contents/BasePlayer.h
+++ GameServer/include/Contents/BasePlayer.h
@@ -1,5 +1,26 @@
 #pragma once
-class BasePlayer
+#include <NetworkLib.h>
+#include "./Contents/Entity.h"
+
+class Sector;
+
+class BasePlayer : public Entity
 {
+public :
+	BasePlayer();
+	~BasePlayer();
+
+	bool IsMove();
+	garam::net::ClientInfo* GetClientInfo();
+	virtual void OnSectorChanged(std::vector<Sector*>& leave, std::vector<Sector*>& enter);
+
+protected:	
+	garam::net::ClientInfo* mClient;
+
+private:
+	//void ProcessLeaveSector(std::vector<Sector*>& leaveSectors);
+	//void ProcessNewEnterSector(std::vector<Sector*>& enterSectors);	
+
+	bool mIsMoving;
 };
 
